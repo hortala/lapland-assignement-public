f4eabd7f06f4acb121c65a4d39e343047e648916
hortala <florent.hortala@imerir.com>
2019-10-15 14:34:37 +0200

    edffr

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/index/');
-	exit;
-?>
+<?php include_once("index.html"); ?>
