6d4e2e13ac13e63599db11a6890cc4af653e32a7
hortala <florent.hortala@imerir.com>
2019-10-16 15:31:08 +0200

    ajustement

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/*Begin NavBar*/ ! function(e) {
+    "use strict";
+    e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() { if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) { var a = e(this.hash); if ((a = a.length ? a : e("[name=" + this.hash.slice(1) + "]")).length) return e("html, body").animate({ scrollTop: a.offset().top - 70 }, 1e3, "easeInOutExpo"), !1 } }), e(".js-scroll-trigger").click(function() { e(".navbar-collapse").collapse("hide") }), e("body").scrollspy({ target: "#mainNav", offset: 100 });
+
+    function a() { 100 < e("#mainNav").offset().top ? e("#mainNav").addClass("navbar-shrink") : e("#mainNav").removeClass("navbar-shrink") }
+    a(), e(window).scroll(a)
+}(jQuery);
+/*End NavBar*/
+
 function clickImageHome(i) {
     if (i > 2) { // i =? 3 or 4 or 5
         if (i == 3) { // i == 3
